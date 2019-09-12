68bcb7258607fdc9e3dd2a3a4e080c7c7b7b9b5a
40huo <git@40huo.cn>
2019-09-12 18:06:45 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 setup(
-    name="dongfeng-base",
+    name="dongfeng_base",
     version="0.0.1",
     packages=find_packages(where=".", exclude=("tests",), include=("*",)),
-    install_requires=["loguru"],
+    install_requires=parse_requirements("requirements.txt"),
     url="",
     license="",
     author="40huo",
