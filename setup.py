3a346122b9b0042c90eb32f30da8de1dc5b4fc3c
40huo <git@40huo.cn>
2019-09-12 18:17:12 +0800

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 setup(
     name="dongfeng_base",
     version="0.0.1",
     packages=find_packages(where=".", exclude=("tests",), include=("*",)),
-    install_requires=parse_requirements("requirements.txt", session="dongfeng"),
+    install_requires=".",
     url="",
     license="",
     author="40huo",
