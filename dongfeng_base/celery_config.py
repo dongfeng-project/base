2a9e7ff28c659e130799f6fd9ab99f031148b8c5
40huo <git@40huo.cn>
2019-09-23 00:28:25 +0800

    定时上报

--- dongfeng_base/celery_config.py
+++ dongfeng_base/celery_config.py
@@ -1,4 +1,4 @@
 from celery.schedules import crontab
 
 # celery beat
-CELERYBEAT_SCHEDULE = {"get_resource_usage": {"task": "tasks.get_resource_usage", "schedule": crontab(second="*/10")}}
+CELERYBEAT_SCHEDULE = {"get_resource_usage": {"task": "dongfeng_base.tasks.misc.get_resource_usage", "schedule": crontab(minute="*/1")}}
