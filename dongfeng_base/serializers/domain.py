8b81f6400a73cb7f9015e1f53f51adbf68528a4e
40huo <git@40huo.cn>
2019-11-02 01:30:33 +0800

    添加子域名title信息serializer

--- dongfeng_base/serializers/domain.py
+++ dongfeng_base/serializers/domain.py
@@ -18,3 +18,12 @@ class EnumSubDomainSerializer(BaseSerializer):
     def __init__(self, domain: str, subdomain_list: List[SubDomainSerializer] = None):
         self.domain = domain
         self.subdomain_list = subdomain_list if subdomain_list else None
+
+
+class SubDomainTitleSerializer(BaseSerializer):
+    def __init__(self, subdomain: str, status_code: int, title: str, content: str, is_match: bool = False):
+        self.subdomain = subdomain
+        self.status_code = int(status_code)
+        self.title = title
+        self.content = content
+        self.is_match = is_match
